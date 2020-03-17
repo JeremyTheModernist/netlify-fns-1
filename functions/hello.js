f2e816d770162c6fa527e10d6cac139324f54bf4
Jeremy Barnes <jeremyb1988@gmail.com>
2020-03-17 09:20:39 -0700

    first netlify function.

---
+++ functions/hello.js
@@ -0,0 +1,12 @@
+function logSuccess() {
+	console.log("successfully called your Netlify Function");
+}
+
+exports.hello = async (event, context, callback) => {
+	const subject = event.queryStringParameters.name || "world";
+	logSuccess();
+	return {
+		statusCode: 200,
+		body: `Hello ${subject}`
+	};
+};
